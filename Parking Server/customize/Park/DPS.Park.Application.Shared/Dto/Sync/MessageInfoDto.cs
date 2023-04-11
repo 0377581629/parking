42668a9dd7394f2bd7ffa15d752fae9ba1f56c0a
0377581629 <quanghuy2152@gmail.com>
2023-04-11 21:33:55 +0700

    delete unused file

--- Parking Server/customize/Park/DPS.Park.Application.Shared/Dto/Sync/MessageInfoDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DPS.Park.Application.Shared.Dto.Sync
-{
-    public class MessageInfoDto
-    {
-        public string PhoneToSendMessage { get; set; }
-        
-        public string MessageContent { get; set; }
-    }
-}
